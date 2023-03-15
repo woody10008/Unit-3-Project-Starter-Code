0bab21d62abe7c8b1d70e3b032b4b739023b604d
woody10008 <127964289+woody10008@users.noreply.github.com>
2023-03-15 09:18:09 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,27 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+
+
+
+
+
+
+
+
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
+
+
